c35ecdaf50a8cccaa57f1ac7ea035d427212e5bc
Maks <vrnmaksim@mail.ru>
2023-01-04 19:46:08 +0300

    add ability encrypt any file

--- AES/cipher.go
+++ AES/cipher.go
@@ -1,19 +1,103 @@
 package AES
 
-func Encrypt (key, plain []byte) (enc []byte){
-	var w [nb*(nr+1)]uint32
+import (
+	"encoding/binary"
+	"errors"
+	"strings"
+)
+
+func Encrypt(key, plain, ext []byte) ([]byte, error) {
+	// Ключ 128 бит (16 байт)
+	if len(key) != 16 {
+		return nil, errors.New("the key is not supported")
+	}
+	sizePlainText := len(plain) // Каждый блок 1 байт => общий размер данных в байтах
+	// TODO: пока максимальный размер только int32, в дальнейшем увеличится
+
+	// Размер не больше размерности для int32 - 15 (т.е ~ 2Гб)
+	if sizePlainText > 2147483632 {
+		return nil, errors.New("large size of the encrypted file")
+	}
+
+	// -------------------------------AES------------------------------------------------
+	// Расширения ключа для AES
+	var w [nb * (nr + 1)]uint32
 	expandKey(key, &w)
 
-	enc = encryptBlock(w, plain)
+	// Шифрование по блокам 16 байт
+	var l, r int = 0, 16
+	enc := make([]byte, 0) // Зашифрованные данные
+	for ;r <= sizePlainText;{
+		encBlock := encryptBlock(w, plain[l:r])
+		enc = append(enc, encBlock...) // TODO: Каждый раз пересоздает слайс
+		l += 16; r += 16 
+	}
 
-	return enc
+	// В последний блок добавляется нули, если не хватает размерности
+	remains := 16 - sizePlainText % 16
+	if remains != 16 {
+		encBlock := encryptBlock(w, append(plain[l:sizePlainText], make([]byte, remains)...))
+		enc = append(enc, encBlock...) 
+	}
+	// ----------------------------------------------------------------------------------
+
+	// Размерность и расширение шифруются в послденем блоке
+	// Размерность занимает 8 байт, расширение 8 байт
+	// Недостающие значение заполняются нулями в начале
+
+	// Переводится в массив [8]byte (8 байт всего)
+	size := make([]byte, 8)
+	binary.BigEndian.PutUint64(size, uint64(sizePlainText))
+	
+	// Дополнение нулями в начале, чтобы размерность расширение была 8 байт
+	ext = append(make([]byte, 8 - len(ext)), ext...)
+	
+	encBlock := encryptBlock(w, append(size, ext...))
+	enc = append(enc, encBlock...)
+
+	return enc, nil
 }
 
-func Decrypt (key, enc []byte) (plain []byte){
-	var w [nb*(nr+1)]uint32
+func Decrypt(key, enc []byte) ([]byte, string, error) {
+	// Ключ 128 бит (16 байт)
+	if len(key) != 16 {
+		return nil, "", errors.New("the key is not supported")
+	}
+	// Размер не больше размерности для int32 - 15 (т.е ~ 2Гб)
+	// Размер зашифрованного сообщение всегда кратен 16 
+	if len(enc) > 2147483647 || len(enc) % 16 != 0 {
+		return nil, "", errors.New("large encrypted file or is it corrupted")
+	}
+
+	var sizePlainText uint64
+	var ext string
+
+	// -------------------------------AES------------------------------------------------
+	// Расширения ключа для AES
+	var w [nb * (nr + 1)]uint32
 	expandKey(key, &w)
 
-	plain = decryptBlock(w, enc)
+	// Поиск из последнего блока: размера исходных данных и расширение файла
+	plainBlock := decryptBlock(w, enc[len(enc)-16:])
+	sizePlainText = binary.BigEndian.Uint64(plainBlock[0:8])
+	ext = strings.Replace(string(plainBlock[8:16]), "\x00" , "", -1)
 
-	return plain
+	// Расшифрование по блокам 16 байт
+	var l, r uint64 = 0, 16
+	plain := make([]byte, 0)
+	for ;r <= sizePlainText;{
+		plainBlock = decryptBlock(w, enc[l:r])
+		plain = append(plain, plainBlock...) // TODO: Каждый раз пересоздает слайс
+		l += 16; r += 16 
+	}
+
+	// Последний блок
+	remains := int(sizePlainText % 16)
+	if remains != 0 {
+		plainBlock := decryptBlock(w, enc[l:r])
+		plain = append(plain, plainBlock[0:remains]...) 
+	}
+	// ----------------------------------------------------------------------------------
+
+	return plain, ext, nil
 }
--- main.go
+++ main.go
@@ -1,64 +1,57 @@
 package main
 
 import (
-	// "bytes"
-	// "encoding/binary"
-
-	// "log"
-	// "os"
-	// "strconv"
-
-	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+	//"fmt"
 
 	"github.com/Big0ak/AES/AES"
 )
 
 func main() {
-	//var word_key string = "qwertyuiasdfghjk"
-	key_byte := [16]byte{ 0x2b, 0x7e, 0x15, 0x16,
-						0x28, 0xae, 0xd2, 0xa6,
-						0xab, 0xf7, 0x15, 0x88,
-						0x09, 0xcf, 0x4f, 0x3c}
-	
-	text_byte := [16]byte{ 0x32, 0x88, 0x31, 0xe0,
-						0x43, 0x5a, 0x31, 0x37,
-						0xf6, 0x30, 0x98, 0x07,
-						0xa8, 0x8d, 0xa2, 0x34 };	
+	// key_byte := [16]byte{ 0x2b, 0x7e, 0x15, 0x16,
+	// 					0x28, 0xae, 0xd2, 0xa6,
+	// 					0xab, 0xf7, 0x15, 0x88,
+	// 					0x09, 0xcf, 0x4f, 0x3c}
+	// text_byte := [16]byte{ 0x32, 0x88, 0x31, 0xe0,
+	// 					0x43, 0x5a, 0x31, 0x37,
+	// 					0xf6, 0x30, 0x98, 0x07,
+	// 					0xa8, 0x8d, 0xa2, 0x34 };	
+	// enc_byte := AES.Encrypt(key_byte[:], text_byte[:])
+	// plain_byte := AES.Decrypt(key_byte[:], enc_byte[:])
+	// fmt.Print(plain_byte)
 
-	enc_byte := AES.Encrypt(key_byte[:], text_byte[:])
-	plain_byte := AES.Decrypt(key_byte[:], enc_byte[:])
-	fmt.Print(plain_byte)
-	// file, err := os.Open("text.txt")
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-	// byte := make([]byte, 16)
-	// kol, err := file.Read(byte)
-	// fmt.Print(kol, "\n", byte)
+	key := "4m5n7q8r9t2j3k4n"
+	nameFile := "test.jpg"
+	cipherFile := "chifer.aes"
+	dectyptFile := "decrypt"
 
-	// buf := bytes.NewReader(byte)
-	// var bin float64
-	// err = binary.Read(buf, binary.BigEndian, &bin)
-	// fmt.Print(bin, "\n")
+	plain, err := os.ReadFile(nameFile)
+	if err != nil {
+		log.Fatal(err)
+	}
+	enc_byte, err := AES.Encrypt([]byte(key), plain[:], []byte(filepath.Ext("/" + nameFile)))
+	if err == nil {
+		file, _ := os.Create(cipherFile)
+		file.Write(enc_byte)
+		file.Close()
+	} else {
+		log.Fatal(err)
+	}
 
-	// fi, err := file.Stat()
-	// fmt.Print(fi.Size())
-	// file.Close()
-	
-	// /////////////////////////////////////////////
-	// file, err = os.Create("binary.bin")
-	// file.WriteString(strconv.FormatFloat(bin, 'f', 6, 64))
-	// file.Close()
-	// ///////////////////////////////////////
+	// ----------------------------------------------------------------------------------
 
-	// buf2 := new(bytes.Buffer)
-	// err = binary.Write(buf2, binary.BigEndian, bin)
-	// if err != nil {
-	// 	log.Fatal("binary.Write failed:", err)
-	// }
-	// fmt.Print(buf2.Bytes())
-
-	// file, err = os.Create("test.txt")
-	// kol, err = file.Write(buf2.Bytes())
-	// file.Close()
+	enc, err := os.ReadFile(cipherFile)
+	if err != nil {
+		log.Fatal(err)
+	}
+	plain_byte, ext, err := AES.Decrypt([]byte(key), enc[:])
+	if err == nil {
+		file, _ := os.Create(dectyptFile+ext)
+		file.Write(plain_byte)
+		file.Close()
+	} else {
+		log.Fatal(err)
+	}
 }
